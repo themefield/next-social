beefb38edca9c078b55f70a06969643da2be87d1
samuel <samuel@westexcel.com>
2024-09-16 09:04:11 -0700

    2024.09.16

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -48,6 +48,10 @@ const Navbar = () => {
             <span>Stories</span>
           </Link>
         </div>
+        <div className="hidden xl:flex p-2 bg-slate-100 items-center rounded-xl">
+          <input type="text" placeholder="search..." className="bg-transparent outline-none" />
+          <Image src="/search.png" alt="" width={14} height={14} />
+        </div>
       </div>
 
       {/* right */}
@@ -55,26 +59,28 @@ const Navbar = () => {
         <ClerkLoading>
           <div className="inline-block h-4 w-4 animate-spin rounded-full border-2 border-gray-500 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white" />
         </ClerkLoading>
-        <ClerkLoaded>
-          <SignedIn>
-            <div className="cursor-pointer">
-              <Image src="/people.png" alt="" width={24} height={24} />
-            </div>
-            <div className="cursor-pointer">
-              <Image src="/messages.png" alt="" width={20} height={20} />
-            </div>
-            <div className="cursor-pointer">
-              <Image src="/notifications.png" alt="" width={20} height={20} />
-            </div>
-            <UserButton />            
-          </SignedIn>
-          <SignedOut>
-            <div className="flex items-center gap-2 text-sm">
-              <Image src="/login.png" alt="" width={20} height={20} />
-              <Link href="/sign-in">Login/Register</Link>
-            </div>
-          </SignedOut>
-        </ClerkLoaded>
+        <div className="hidden md:block">
+          <ClerkLoaded>
+            <SignedIn>
+              <div className="cursor-pointer">
+                <Image src="/people.png" alt="" width={24} height={24} />
+              </div>
+              <div className="cursor-pointer">
+                <Image src="/messages.png" alt="" width={20} height={20} />
+              </div>
+              <div className="cursor-pointer">
+                <Image src="/notifications.png" alt="" width={20} height={20} />
+              </div>
+              <UserButton />            
+            </SignedIn>
+            <SignedOut>
+              <div className="flex items-center gap-2 text-sm">
+                <Image src="/login.png" alt="" width={20} height={20} />
+                <Link href="/sign-in">Login/Register</Link>
+              </div>
+            </SignedOut>
+          </ClerkLoaded>
+        </div>
         <MobileMenu />
       </div>
     </div>
---
+++ src/components/Stories.tsx
@@ -0,0 +1,7 @@
+const Stories = () => {
+  return (
+    <div className='p-4 bg-white rounded-lg shadow-md overflow-scroll text-xs'>Stories</div>
+  );
+};
+
+export default Stories;
